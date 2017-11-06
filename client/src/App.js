8bb35c9aeddff6ab454665d2f108ca0aaef00b32
Vit Benton <vitbenton@Macintosh.local>
2017-11-05 21:06:47 -0500

    migrated all rendering to Articles.js. Started API util

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Main from './components/Main/Main.js';
+import Articles from './Articles';
 
-const App = () => (<Main/>);
+const App = () => (<Articles/>);
 
 export default App;
