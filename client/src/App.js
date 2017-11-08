cfbdf4bb78efb682b4fdd874867f145bee684736
Vit Benton <vitbenton@Macintosh.local>
2017-11-07 23:24:40 -0500

    added react router with required dependencies, started on saved compenets and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Main from "./components/Main/Main";
+import Saved from "./components/Saved/Saved";
 
-const App = () => (<Main />);
+const App = () =>
+<Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/saved" component={Saved} />
+      </Switch>
+    </div>
+  </Router>;
 
 export default App;
--- client/src/components/Saved/Saved.js
+++ client/src/components/Saved/Saved.js
@@ -1,1 +1,14 @@
 import React, { Component } from "react";
+import Nav from "../Main/Nav";
+
+class Saved extends Component {
+
+  render() {
+    return (
+    <Nav />
+
+    );
+  }
+}
+
+export default Saved;
